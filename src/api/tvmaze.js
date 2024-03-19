5836e777457217959e7bd0c193b58c818c663172
shrey parekh <your_email@example.com>
2024-03-19 17:05:42 +0530

    fetched starred shows and display shoegrid for them

--- src/api/tvmaze.js
+++ src/api/tvmaze.js
@@ -9,3 +9,10 @@ export const searchForShows = query => apiGet(`/search/shows?q=${query}`);
 export const searchForPeople = query => apiGet(`/search/people?q=${query}`);
 export const getShowById = ShowId =>
   apiGet(`/shows/${ShowId}?embed[]=seasons&embed[]=cast`);
+
+export const getShowsByIds = async showIds => {
+  const promises = showIds.map(showId => apiGet(`/shows/${showId}`));
+  return Promise.all(promises);
+};
+// THOS CODE IS USED WHEN ALL THE DATA IS WHICH IS IN ARRY ARE CALLED TOGTHER  AND IT HELPS TO CALL SPECIFIC FUNTION WHEN IN USE
+//Promise.all(). The function returns a single promise that resolves with an array of results once all API calls are completed..
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -1,8 +1,29 @@
 import { useStarredShows } from '../lib/useStarredShows';
+import { useQuery } from '@tanstack/react-query';
+import ShowGrid from '../components/shows/ShowGrid';
+import { getShowsByIds } from '../api/tvmaze';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
+  const [starredShowsIds] = useStarredShows();
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowsByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
 
-  return <div>Starred page, starred {starredShows.length}</div>;
+  if (starredShows?.length === 0) {
+    return <div>No shows were starred</div>;
+  }
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+  if (starredShowsError) {
+    return <div>Error occured: {starredShowsError.message}</div>;
+  }
+  return <div> Shows are loading</div>;
 };
 export default Starred;
+//In summary, this component fetches data about starred shows asynchronously, handles different states (e.g., loading, error, no starred shows), and renders the appropriate UI based on the fetched data and any potential errors.
